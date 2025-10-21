c79210808c796e5e0bd02dbcfe91e7f40a8f0d7d
gokm8 <gouzb22@student.sdu.dk>
2025-10-21 15:34:02 +0200

    commit: add vores mission page

---
+++ app/vores-mission/_components/Header.tsx
@@ -0,0 +1,16 @@
+import { Container, Section } from "@/components/ds";
+
+export const Header = () => {
+    return (
+        <Section>
+            <Container>
+                <h1 className="text-2xl font-bold">Vores mission</h1>
+                <p className="text-base">
+                    Lorem ipsum dolor sit amet, consectetur adipisicing elit,
+                    sed do eiusmod tempor incididunt ut labore et dolore magna
+                    aliqua.
+                </p>
+            </Container>
+        </Section>
+    );
+};
---
+++ app/vores-mission/page.tsx
@@ -0,0 +1,10 @@
+import { Main } from "@/components/ds";
+import { Header } from "./_components/Header";
+
+export default function VoresMission() {
+    return (
+        <Main>
+            <Header />
+        </Main>
+    );
+}
