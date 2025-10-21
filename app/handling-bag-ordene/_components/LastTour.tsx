f6e46953690b482ed7e32f340bcf8623512629d2
gokm8 <gouzb22@student.sdu.dk>
2025-10-21 14:40:11 +0200

    adding gallery

--- app/handling-bag-ordene/_components/LastTour.tsx
+++ app/handling-bag-ordene/_components/LastTour.tsx
@@ -1,13 +1,50 @@
 import { Container, Section } from "@/components/ds";
+import Image from "next/image";
 
 export const LastTour = () => {
     return (
         <Section>
             <Container>
-                <h2 className="text-xl font-bold">Sidste tur</h2>
+                <h2 className="text-xl font-bold">
+                    Vores tidligere rejse til Østtyrkiet
+                </h2>
                 <p className="text-base">
-                    Her kan du finde information om vores sidste tur.
+                    Lorem ipsum dolor sit amet, consectetur adipisicing elit,
+                    sed do eiusmod tempor incididunt ut labore et dolore magna
+                    aliqua.
                 </p>
+            </Container>
+            <Container>
+                <div className="grid gap-4">
+                    <Image
+                        src="/poverty.jpg"
+                        alt="Last Tour"
+                        width={1000}
+                        height={1000}
+                        className="rounded-lg"
+                    />
+                    <Image
+                        src="/poverty.jpg"
+                        alt="Last Tour"
+                        width={1000}
+                        height={1000}
+                        className="rounded-lg"
+                    />
+                    <Image
+                        src="/poverty.jpg"
+                        alt="Last Tour"
+                        width={1000}
+                        height={1000}
+                        className="rounded-lg"
+                    />
+                    <Image
+                        src="/poverty.jpg"
+                        alt="Last Tour"
+                        width={1000}
+                        height={1000}
+                        className="rounded-lg"
+                    />
+                </div>
             </Container>
         </Section>
     );
