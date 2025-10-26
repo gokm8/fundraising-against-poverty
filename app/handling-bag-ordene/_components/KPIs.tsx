2ad5631d7ee1127b4214b865c4f946665e4bead2
gokm8 <gouzb22@student.sdu.dk>
2025-10-26 21:04:10 +0100

    fixing KPIs

--- app/handling-bag-ordene/_components/KPIs.tsx
+++ app/handling-bag-ordene/_components/KPIs.tsx
@@ -1,29 +1,44 @@
 import { Container, Section } from "@/components/ds";
-import { HandCoins } from "lucide-react";
-import { Users } from "lucide-react";
-import { HouseHeart } from "lucide-react";
+import { HandCoins, Users, HouseHeart } from "lucide-react";
+
+const kpis = [
+    {
+        icon: HandCoins,
+        label: "73.580 DKK indsamlet",
+    },
+    {
+        icon: Users,
+        label: "1.200 børn støttet",
+    },
+    {
+        icon: HouseHeart,
+        label: "8 landsbyer hjulpet",
+    },
+];
 
 export const KPIs = () => {
     return (
         <Section className="bg-muted">
             <Container className="max-w-5xl">
-                <div className="flex flex-col gap-4 items-center justify-center text-center">
-                    <h2 className="text-base font-bold">
+                <div className="flex flex-col gap-6 items-center justify-center text-center">
+                    <h2 className="text-lg font-bold md:text-2xl ">
                         Vi har allerede gjort en forskel
                     </h2>
-                    <div className="flex flex-col md:flex-row text-sm justify-between w-full gap-4">
-                        <div className="flex flex-col md:flex-row items-center justify-center gap-2">
-                            <HandCoins className="w-8 h-8" />
-                            <p>73.580 DKK indsamlet</p>
-                        </div>
-                        <div className="flex flex-col md:flex-row items-center justify-center gap-2">
-                            <Users className="w-8 h-8" />
-                            <p>1.200 børn støttet</p>
-                        </div>
-                        <div className="flex flex-col md:flex-row items-center justify-center gap-2">
-                            <HouseHeart className="w-8 h-8" />
-                            <p>8 landsbyer hjulpet</p>
-                        </div>
+
+                    <div className="flex flex-col md:flex-row justify-between w-full gap-4">
+                        {kpis.map((item, index) => {
+                            const Icon = item.icon;
+                            return (
+                                <div
+                                    key={index}
+                                    className="flex flex-col md:flex-row items-center justify-center gap-2">
+                                    <Icon className="w-8 h-8 text-muted-foreground" />
+                                    <p className="text-base font-semibold text-muted-foreground">
+                                        {item.label}
+                                    </p>
+                                </div>
+                            );
+                        })}
                     </div>
                 </div>
             </Container>
